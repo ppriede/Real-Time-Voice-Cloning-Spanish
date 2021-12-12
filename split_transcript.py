e8e79bd6f3e8cd10db8dce09d29b79509ef2cdad
schunga@uni.pe <schunga@uni.pe>
2021-12-12 09:22:08 -0500

    Script to prepare Spanish Dataset

--- split_transcript.py
+++ split_transcript.py
@@ -13,21 +13,24 @@ def split_tux100h():
                 output_file.write(texts[i])
 
 def split_cvcorpus():
-    with Path("cv-corpus-7.0-2021-07-21/es/validated.txt").open("r", encoding="utf8") as metadata_file:
+    with Path("D:/tesis2/cv-corpus-7.0-2021-07-21/es/train.tsv").open("r", encoding="utf8") as metadata_file:
         metadata = [line.split("\t") for line in metadata_file]
-        texts = [item[2] for item in metadata]
-        audio_filenames = ["cv-corpus-7.0-2021-07-21_validated/" + item[1] + ".txt" for item in metadata]
+        texts = [item[2] for item in metadata[1:]]
+
+        audio_filenames = [item[1] for item in metadata[1:]]
+        filename_parts = [filename.split(".") for filename in audio_filenames]
+        names = [part[0] for part in filename_parts]
+        text_filenames = ["datasets_root/cvcorpus/valid/spanish/cv-corpus/" + name + ".txt" for name in names]
         
         for i in tqdm(range(len(audio_filenames))):
-            with Path(audio_filenames[i]).open("w", encoding="utf8") as output_file:
-                if audio_filenames[i] != "path":
-                    output_file.write(texts[i])
+            with Path(text_filenames[i]).open("w", encoding="utf8") as output_file:
+                output_file.write(texts[i])
         
-        for name in audio_filenames:
+        for name in names:
             shutil.copyfile(
-                'cv-corpus-7.0-2021-07-21/es/clips/'+name+'.mp3',
-                'cv-corpus-7.0-2021-07-21_validated/'+name+'.mp3'
-            )
+                    'D:/tesis2/cv-corpus-7.0-2021-07-21/es/clips/'+name+'.mp3',
+                    'datasets_root/cvcorpus/valid/spanish/cv-corpus/'+name+'.mp3'
+                )
 
 def split_pespa(gender):
      with Path("datasets_root/PeruvianSpanish/train/peruvianvoices/es_pe_"+gender+"/line_index.tsv").open("r", encoding="utf8") as metadata_file:
@@ -47,5 +50,6 @@ if __name__=="__main__":
     #            'datasets_root/tux100h-cvcorpus/valid/spanish/tux-100h_new/utterance-'+str(i)+'.wav'
     #        )
     #split_tux100h()
-    split_pespa("female")
-    split_pespa("male")
+    #split_pespa("female")
+    #split_pespa("male")
+    split_cvcorpus()

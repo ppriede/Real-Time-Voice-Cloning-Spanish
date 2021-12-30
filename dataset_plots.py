1874bbe1b196f21c72a459983d17f47cdd4fef65
schunga@uni.pe <schunga@uni.pe>
2021-12-30 01:35:24 -0500

    Script to generate plots from Spanish Dataset

---
+++ dataset_plots.py
@@ -0,0 +1,23 @@
+from matplotlib import pyplot as plt
+from collections import Counter
+with open("D:/tesis2/cv-corpus-7.0-2021-07-21/es/train.tsv") as f:
+    headers = f.readline()[:-1].split('\t')
+    data = []
+    for row in f:
+        d = { header:value for header,value in zip(headers,row[:-1].split('\t')) }
+        data.append(d)
+
+
+subdata = [ d['accent'] for d in data ]
+labels,heights = zip(*Counter(subdata).items())
+plt.bar(range(len(heights)),heights)
+plt.xticks(range(len(labels)), labels, rotation='vertical')
+plt.title(header)
+plt.show()
+
+subdata = [ d['gender'] for d in data ]
+labels,heights = zip(*Counter(subdata).items())
+plt.pie(heights)
+plt.legend(labels)
+plt.title('gender')
+plt.show()
